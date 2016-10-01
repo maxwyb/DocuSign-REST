fdd67cfc402df23bebdde62ac53ea6d50a66a7c7
Yingbo (Max) Wang <maxwyb@gmail.com>
2016-10-01 15:39:37 -0700

    Get account URL HTTP parsing - initial attempt.

--- DocuSign-REST/ViewController.swift
+++ DocuSign-REST/ViewController.swift
@@ -21,8 +21,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func buttonClicked(sender: AnyObject) {
+        getAccountURL()
+    }
     
+    func getAccountURL() {
+        //let parameters: Parameters = []
+        let userVerification = ["Username":"maxwyb@hotmail.com", "Password":"NNbnds.2009", "IntegratorKey":"7da1c5dc-5fae-4df9-80b4-4dbeffd8db34"]
+        //var error: NSError?
+        /*
+        if let data = JSONSerialization.dataWithJSONObject(userVerification, options: JSONSerialization.WritingOptions.PrettyPrinted) {
+            if let json = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                print(json)
+                
+                let headers: HTTPHeaders = [ "X-DocuSign-Authentication": json]
+                Alamofire.request("https://demo.docusign.net/restapi/v2/login_information", headers: headers).responseJSON { response in
+                    debugPrint(response)
+                }
+            }
+        }
+        */
+        /*
+        var data: Data
+        do {
+            data = try JSONSerialization.data(withJSONObject: userVerification, options: JSONSerialization.WritingOptions.prettyPrinted)
+        } catch _ {
+            debugPrint("Error...");
+        }
+        if let data = data as Data? {
+            let json = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+            if let json = json {
+                print(json)
+            }
+        }
+        */
+        var data: Data?
+        do {
+            data = try JSONSerialization.data(withJSONObject: userVerification, options: JSONSerialization.WritingOptions.prettyPrinted)
+        } catch {
+            data = nil
+        }
+        if let data = data {
+            let json = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+            if let json = json {
+                debugPrint(json)
+                
+                let headers: HTTPHeaders = [ "X-DocuSign-Authentication": json as String]
+                Alamofire.request("https://demo.docusign.net/restapi/v2/login_information", headers: headers).responseJSON { response in
+                    debugPrint(response)
+                }
+            }
+        }
 
+    }
 
 }
 
