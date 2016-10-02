733eff51d0ad5e0c553fcabf5200ff2e2e9f07b1
Yingbo (Max) Wang <maxwyb@gmail.com>
2016-10-02 01:15:44 -0700

    Initial implementation of sending signature request.

--- DocuSign-REST/ViewController.swift
+++ DocuSign-REST/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 import Alamofire
 
+let userVerification = ["Username":"maxwyb@hotmail.com", "Password":"ab12cd34", "IntegratorKey":"7da1c5dc-5fae-4df9-80b4-4dbeffd8db34"]
+
+var userJSON: NSString?
+
+var userHeader: HTTPHeaders?
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -24,13 +31,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel?
     
     @IBAction func buttonClicked(sender: AnyObject) {
+        calculateUserHeader()
         getAccountURL()
+        requestEnvelop()
     }
     
-    func getAccountURL() {
-
-        let userVerification = ["Username":"maxwyb@hotmail.com", "Password":"NNbnds.2009", "IntegratorKey":"7da1c5dc-5fae-4df9-80b4-4dbeffd8db34"]
-
+    func calculateUserHeader() {
+        /* generate a string header with three nested fields */
         var data: Data? // NSDictionary to NSData Serialization
         do {
             data = try JSONSerialization.data(withJSONObject: userVerification)
@@ -42,53 +49,95 @@ class ViewController: UIViewController {
             let json = NSString(data: data, encoding: String.Encoding.utf8.rawValue) // NSData to NSString
             if let json = json {
                 //debugPrint(json)
-                
-                
-                let headers: HTTPHeaders = [ "X-DocuSign-Authentication": json as String]
-                Alamofire.request("https://demo.docusign.net/restapi/v2/login_information", headers: headers).responseJSON { response in
-                    
-                    /*
-                    debugPrint("----------response.data----------")
-                    debugPrint(response.data)
-                    debugPrint("---------------------------------")
-                    debugPrint("----------response.description----------");
-                    debugPrint(response.description)
-                    debugPrint("---------------------------------")
-                    debugPrint("----------response.result----------");
-                    debugPrint(response.result)
-                    debugPrint("---------------------------------")
-                    debugPrint("----------response.result.value----------");
-                    debugPrint(response.result.value)
-                    */
- 
-                    let loginAccounts = response.result.value as! NSDictionary
-                    /*
-                    let loginAccounts_login = loginAccounts["loginAccounts"]
-                    print(loginAccounts_login)
-                    */
-                    /*
-                    let dicExample = ["Apple": 1, "Banana": 12]
-                    print(dicExample);
-                    */
-                    let loginAccounts_login = (loginAccounts["loginAccounts"]! as! NSArray).mutableCopy() as! NSMutableArray
-                    let loginAccounts_login_dict = loginAccounts_login[0] as! NSDictionary
-                    let userID = loginAccounts_login_dict["userId"]
-                    
-                    self.label!.text = userID as! String
-                    
-                    
-                    /*
-                    var jsonParsed: [String:String]?
-                    do {
-                        try jsonParsed = JSONSerialization.jsonObject(with: response.result, options: []) as? [String:String]
-                    } catch let error as NSError {
-                        print(error)
-                    }
-                    */
-                }
+                userJSON = json
+                userHeader = [ "X-DocuSign-Authentication": json as String]
             }
+        }
+        
+    }
+    
+    func getAccountURL() {
+
+        /* Parse the userID */
+        Alamofire.request("https://demo.docusign.net/restapi/v2/login_information", headers: userHeader).responseJSON { response in
+            
+            /*
+            debugPrint("----------response.data----------")
+            debugPrint(response.data)
+            debugPrint("---------------------------------")
+            debugPrint("----------response.description----------");
+            debugPrint(response.description)
+            debugPrint("---------------------------------")
+            debugPrint("----------response.result----------");
+            debugPrint(response.result)
+            debugPrint("---------------------------------")
+            debugPrint("----------response.result.value----------");
+            debugPrint(response.result.value)
+            */
+
+            let loginAccounts = response.result.value as! NSDictionary
+            /*
+            let loginAccounts_login = loginAccounts["loginAccounts"]
+            print(loginAccounts_login)
+            */
+            /*
+            let dicExample = ["Apple": 1, "Banana": 12]
+            print(dicExample);
+            */
+            let loginAccounts_login = (loginAccounts["loginAccounts"]! as! NSArray).mutableCopy() as! NSMutableArray
+            let loginAccounts_login_dict = loginAccounts_login[0] as! NSDictionary
+            let userID = loginAccounts_login_dict["userId"]
+            
+            self.label!.text = userID as! String
+            
+            /*
+            var jsonParsed: [String:String]?
+            do {
+                try jsonParsed = JSONSerialization.jsonObject(with: response.result, options: []) as? [String:String]
+            } catch let error as NSError {
+                print(error)
+            }
+            */
         }
     }
 
+    func requestEnvelop() {
+        /* send HTTP post request for document signature */
+        let postWeb: String = "https://demo.docusign.net/restapi/v2/accounts/1703061/envelopes"
+        
+        let headers: HTTPHeaders = ["X-DocuSign-Authentication" : userJSON as! String, "Accept" : "application/json", "Content-Type" : "application/json"]
+        
+        let parameters: Parameters = ["X-DocuSign-Authentication" : userJSON as! String,
+                                      "Accept" : "application/json",
+                                      "Content-Type" : "application/json",
+                                      "emailSubject" : "DocuSign REST API from Swift 3!",
+                                      "emailBlurb" : "Shows how to create and send an envelope from a document.",
+                                      "recipients" : [
+                                        "signers" : [[
+                                            "email" : "maxwyb@gmail.com",
+                                            "name" : "Max W.",
+                                            "recipientId" : "1",
+                                            "routingOrder" : "1"
+                                        ]]
+                                      ],
+                                      "documents" : [[
+                                        "documentId" : "1",
+                                        "name" : "sd-hacks.pdf",
+                                        "documentBase64" : "<base64 encoded document bytes>"
+                                       ]],
+                                      "status" : "sent"
+        ]
+        
+        Alamofire.request(postWeb, method: .post, headers: headers).responseJSON { response in
+            debugPrint("--------header starts.------------------------")
+            debugPrint(response)
+            debugPrint("--------header ends; parameter starts.--------")
+        }
+        
+        Alamofire.request(postWeb, method: .post, parameters: parameters).responseJSON { response in
+            debugPrint(response)
+        }
+         
+    }
 }
 
